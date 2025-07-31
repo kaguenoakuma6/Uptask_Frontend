3d6526992333777e9b7c3ea479284de8355226a7
Kaguenoakuma <shadowfocker666@gmail.com>
2025-07-31 11:15:21 -0600

    Base Inicial del FrontEnd del proyecto UpTask

---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,10 @@
+
+
+export default function DashboardView() {
+  return (
+    <>
+      <h1 className="text-5xl font-black">Mis Proyectos</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y Administra tus Proyectos</p>
+    </>
+  )
+}
