33e96dc51a240e4049e7a12ed35272da5a13794d
Kaguenoakuma <shadowfocker666@gmail.com>
2025-08-05 23:25:32 -0600

    Implementacion inicial del Backend

---
+++ src/api/ProjectAPI.ts
@@ -0,0 +1,21 @@
+import api from "@/lib/axios";
+import type { ProjectFormData } from "@/types/index";
+import { isAxiosError } from "axios";
+
+export async function createProject (formData: ProjectFormData)
+{
+    try {
+                
+        const { data }  = await api.post('/projects', formData);
+        return data;
+        
+    } 
+    catch (error) 
+    {
+        if (isAxiosError(error) && error.response) 
+        {
+            throw new Error(error.response.data.error);
+        }
+    }
+    
+}
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 export default function DashboardView() {
@@ -5,6 +6,9 @@ export default function DashboardView() {
     <>
       <h1 className="text-5xl font-black">Mis Proyectos</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">Maneja y Administra tus Proyectos</p>
+      <nav className="my-5">
+        <Link className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors" to={'/projects/create'}>Nuevo Proyecto</Link>
+      </nav>
     </>
   )
 }
